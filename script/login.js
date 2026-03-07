ae55f562231824c07f749ca2380e85fbc11cc77a
Adri-shikhar <adrishikharbarua77452@gmail.com>
2026-03-07 20:49:00 +0600

    implemented the login page and connected it with the other homepage

---
+++ script/login.js
@@ -0,0 +1,12 @@
+document.getElementById('loginButton').addEventListener('click', function(event) {
+   event.preventDefault();
+
+   const username = document.getElementById('username').value;
+   const password = document.getElementById('password').value;
+
+   if (username === 'admin' && password === 'admin123') {
+      window.location.href = './home.html';
+   } else {
+      alert('Invalid username or password. Please try again.');
+   }
+});
